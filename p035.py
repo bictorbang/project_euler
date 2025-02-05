7624e5183fe4f025aa26a14acaea042c9d7bbc32
bictorbang <victor.wang@ensea.fr>
2025-02-05 19:07:29 +0100

    fix: p035

--- p035.py
+++ p035.py
@@ -6,6 +6,7 @@ from collections import deque
 def circular_primes(n = 1_000_000):
     total = 0
     for prime in list_primes(n):
+        if set(str(prime)) & set("02468"): continue
         nums = (get_rotations(deque(str(prime))))                       
         if all((is_prime(int(''.join(num))) for num in nums)):
             total += 1
@@ -18,6 +19,9 @@ def get_rotations(n: deque):
 
 def compute():
     return str(circular_primes())
-    
+
+import time
 if __name__ == "__main__":
+    timer = time.time()
     print(compute())
+    print(f"Elapsed: {(time.time() - timer)*1000:.3f} ms")
