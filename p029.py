ed8cee03e232e40e4eba0e4cf87d6ea37a8b2257
bictorbang <victor.wang@ensea.fr>
2025-02-05 16:45:29 +0100

    fix: p029

--- p029.py
+++ p029.py
@@ -1,9 +1,7 @@
 # Distinct Powers
 
 def distinct_powers(a1 = 2, a2 = 100, b1 = 2, b2 = 100):
-    powers = set((a**b for a in range(a1, a2 + 1) for b in range(b1, b2 + 1)))
-    return len(powers)
-
+    return len({a**b for a in range(a1, a2 + 1) for b in range(b1, b2 + 1)})
 
 def compute():
     return str(distinct_powers())
